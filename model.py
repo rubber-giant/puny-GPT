c3d6dc50291ba3ae47a2d43237dcb4322b902399
Tanmay Singh <tanmay8903@gmail.com>
2026-08-06 04:34:02 +0530

    Tokenizer setup

--- model.py
+++ model.py
@@ -1,9 +1,24 @@
 from datasets import load_dataset
 
+# load dataset
 ds = load_dataset("roneneldan/TinyStories")
 
 train = ds["train"]
 val = ds["validation"]
 
-print(train[0])
-print(len(train))
+# Vocab setup
+char = sorted(set(''.join(train['text'])))
+vocab_size = len(char)
+
+## Tokenize
+stoi = {ch:i for i,ch in enumerate(char)}
+itos = {i:ch for i,ch in enumerate(char)}
+
+def encode(s):
+    return [stoi[ch] for ch in s]
+
+def decode(i):
+    return (''.join(itos[i] for i in i))
+
+print(encode("hello babygirl"))
+print(decode(encode("hello babygirl")))
